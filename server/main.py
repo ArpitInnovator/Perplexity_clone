c182f27776713b1848fd63e3be70a556da63da7a
Arpit <arpitprofessional04@gmail.com>
2025-08-31 16:20:57 +0530

    Add CORS

--- server/main.py
+++ server/main.py
@@ -4,9 +4,17 @@ from pydantic_models.chat_body import ChatBody
 from services.sort_source_service import SortSourceService
 from services.search_service import SearchService
 from services.llm_service import LLMService
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 search_service = SearchService()
 sort_source_service = SortSourceService()
 llm_service = LLMService()
